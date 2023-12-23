bba706ed4665c2a1f6aea1a64cebc12b8511ed35
iamrk-js <javascriptiscoolstuff@gmail.com>
2023-12-23 13:11:41 +0530

    qq

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,21 @@
 const cl = console.log;
+
+const sideBarToggle = document.getElementById("sideBarToggle");
+const sidebar = document.getElementById("sidebar");
+const sideBarHide = document.querySelector("#sideBarHide");
+
+// const showSideBar = () => {
+//     sidebar.classList.add('active')
+// }
+
+// const hideSideBar = () => {
+//     sidebar.classList.remove('active')
+// }
+
+const toggleSidebar = () => {
+    sidebar.classList.toggle('active')
+}
+
+
+sideBarToggle.addEventListener('click', toggleSidebar);
+sideBarHide.addEventListener("click", toggleSidebar)
